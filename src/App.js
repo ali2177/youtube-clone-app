9010da01e9c9cb603cda0189a9feae756d562cf3
Ali mehr <ali.mehrjui@gmail.com>
2023-05-25 16:02:53 +0330

    sctructuring components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
+import Navbar from "./component/Navbar";
+import Feed from "./component/Feed";
+import VideoDetail from "./component/VideoDetail";
+import ChannelDetail from "./component/ChannelDetail";
+import SearchFeed from "./component/SearchFeed";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" exact element={<VideoDetail />} />
+          <Route path="/channel/:id" exact element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 };
 
