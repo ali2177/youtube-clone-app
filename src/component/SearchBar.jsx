21ea606658a8a8c43a44d82c5c169f79d82c38aa
Ali mehr <ali.mehrjui@gmail.com>
2023-05-26 13:11:51 +0330

    implementing searching for videos

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -4,10 +4,15 @@ import { Paper, IconButton } from "@mui/material";
 import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
+  const [userInput, setUserInput] = useState("");
+  const navigate = useNavigate();
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (userInput) navigate(`/search/${userInput}`);
+      }}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
@@ -19,8 +24,10 @@ const SearchBar = () => {
       <input
         className="search-bar"
         placeholder="Search ..."
-        value=""
-        onChange={() => {}}
+        value={userInput}
+        onChange={(e) => {
+          setUserInput(e.target.value);
+        }}
       />
       <IconButton type="submit" sx={{ p: "10px", color: "red" }}>
         <Search />
--- src/component/SearchFeed.jsx
+++ src/component/SearchFeed.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import Video from "./Video";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflow: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Result For :{" "}
+        <span style={{ color: "#f31503" }}>{searchTerm}</span>
+      </Typography>
+      <Video videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
